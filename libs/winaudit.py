35329f5ec108ca8a2d338d0e515a85a6f408cf2f
bentleygd <gbentley@24hourfit.com>
2020-02-06 13:30:05 -0800

    Minor code fix, added test cases for audit testing.

--- libs/winaudit.py
+++ libs/winaudit.py
@@ -3,7 +3,7 @@ from time import time
 from re import search
 from configparser import ConfigParser
 
-from ldap import initalize, SCOPE_SUBTREE
+from ldap import initialize, SCOPE_SUBTREE
 from ldap import SERVER_DOWN, INVALID_CREDENTIALS, SIZELIMIT_EXCEEDED
 from win32.win32net import NetLocalGroupGetMembers as NLGGM
 
@@ -54,7 +54,7 @@ class ADAudit():
         SIZELIMIT_EXCEEDED - The returned results of a search are
         greater that the LDAP server's max size limit."""
         # Connecting to LDAP server (i.e., a domain controller)
-        ldap_obj = initalize(ldap_dict['ldap_url'])
+        ldap_obj = initialize(ldap_dict['ldap_url'])
         start = time()
         try:
             ldap_obj.simple_bind_s(
@@ -69,11 +69,11 @@ class ADAudit():
             exit(1)
         except SERVER_DOWN:
             self.log_me.exception(
-                'Cannot contact server at: %s', ldap_dict['ldap_url']
+                'Cannot contact server at: %s' % ldap_dict['ldap_url']
             )
             exit(1)
         self.log_me.debug(
-            'Successfully connected to %s', ldap_dict['ldap_url']
+            'Successfully connected to %s' % ldap_dict['ldap_url']
         )
         # Iterating through OUs, appending the servers in those OUs to
         # the host list.
@@ -84,33 +84,32 @@ class ADAudit():
                                       attrsonly=0)
                 )
                 self.log_me.debug(
-                    'Server members of %s succsefully retrieved', ou
+                    'Server members of %s succsefully retrieved' % ou
                 )
                 for server in server_data:
                     self.host_list.append(server[1].get('name')[0])
                 self.log_me.debug(
-                    'Server members of %s added to host list.', ou
+                    'Server members of %s added to host list.' % ou
                 )
             # Some exception handling.
             except SIZELIMIT_EXCEEDED:
                 self.log_me.exception(
-                    'Size limit exceeded reached when searhcing: %s',
-                    ou
+                    'Size limit exceeded reached when searhcing: %s' % ou
                 )
         # Unbinding LDAP object to free up resources.
         ldap_obj.unbind_s()
         end = time()
         _elapsed = end - start
         elapsed = int(round(_elapsed, 0))
-        self.log_me.debug('It took %d seconds to retrieve servers', elapsed)
+        self.log_me.debug('It took %d seconds to retrieve servers' % elapsed)
         self.log_me.info(
-            '%d servers added to audit population.', len(self.host_list)
+            '%d servers added to audit population.' % len(self.host_list)
         )
         # Checking the host list to make sure that it has a reasonable
         # population.
         if len(self.host_list) < 20:
             self.log_me.error(
-                'Server list only contains %d members.', len(self.host_list)
+                'Server list only contains %d members.' % len(self.host_list)
             )
 
     def get_domain_admins(self, ldap_dict, admin_dict):
@@ -132,7 +131,7 @@ class ADAudit():
         INVALID_CREDENTIALS - Fatal exception that is raised when the
         credentials provided in ldap_dict do not log on."""
         # Connecting to LDAP server (i.e., a domain controller)
-        ldap_obj = initalize(ldap_dict['ldap_url'])
+        ldap_obj = initialize(ldap_dict['ldap_url'])
         start = time()
         try:
             ldap_obj.simple_bind_s(
@@ -141,17 +140,17 @@ class ADAudit():
         # Some exception handling.
         except INVALID_CREDENTIALS:
             self.log_me.exception(
-                'Login failure when attempting to connect to: %s',
-                ldap_dict['ldap_url']
+                'Login failure when attempting to connect to: %s' %
+                (ldap_dict['ldap_url'])
             )
             exit(1)
         except SERVER_DOWN:
             self.log_me.exception(
-                'Cannot contact server at: %s', ldap_dict['ldap_url']
+                'Cannot contact server at: %s' % ldap_dict['ldap_url']
             )
             exit(1)
         self.log_me.debug(
-            'Successfully connected to %s', ldap_dict['ldap_url']
+            'Successfully connected to %s' % ldap_dict['ldap_url']
         )
         # Obtaining a list of domain admins.
         self.log_me.debug('Obtaining a list of domain admins.')
@@ -172,7 +171,7 @@ class ADAudit():
         self.log_me.info('Domain admin list successfully generated.')
         _elapsed = end - start
         elapsed = int(round(_elapsed, 0))
-        self.log_me.debug('Domain admins retrieved in %d seconds', elapsed)
+        self.log_me.debug('Domain admins retrieved in %d seconds' % elapsed)
 
     def get_domain_admin_ex(self):
         """Populates self.domain_admin_ex with list of bad admins.
@@ -196,7 +195,7 @@ class ADAudit():
                 # Let's log the fact that there is a bad domain admin.
                 # If anything is log worthy, it is a bad domain admin.
                 self.log_me.warning(
-                    '%s is not an approved domain admin', admin
+                    '%s is not an approved domain admin' % admin
                 )
 
 
@@ -206,11 +205,11 @@ class WinServerAudit(ADAudit):
 
         Attributes:
         self.local_admins = list() , A list of dictionaries containing
-        the following keys: hostname (self explanatory), admins
-        (self explanatory).
+        the following keys: host (self explanatory), admins (self
+        explanatory).
         self.local_admin_ex = list(), A list of dictionaries containing
-        the following keys: hostname (self explanatory), bad_admins
-        (self explanatory).
+        the following keys: host (self explanatory), bad_admins (self
+        explanatory).
 
         Methods:
         get_local_admins - Gets all of the local administrators from the
@@ -225,8 +224,8 @@ class WinServerAudit(ADAudit):
         """Retrieves the local admins from a Windows server.
 
         Outputs:
-        local_admins - list(), A list of users that are in the server's
-        local administrator group.
+        self.local_admins - list(), A list of users that are in the
+        server's local administrator group.
 
         Raises:
         TBD"""
@@ -245,25 +244,27 @@ class WinServerAudit(ADAudit):
             )
 
     def get_local_admin_ex(self):
-        """Populates self.domain_admin_ex with list of bad admins.
+        """Populates self.local_admin_ex with list of bad admins.
 
         Inputs:
-        known_admins - list(), A list of known good admins.
-        admin_list - list(), The current list of domain admins from AD.
+        self.local_admins - list(), an attribue that is populated by
+        get_local_admins.
+
 
         Outputs:
-        self.domain_admin_ex - list(), A list of accounts that are in
-        the domain admin group that do not have appropriate access.
-        If this list is not empty, you have problems."""
+        self.local_admin_ex - list(), A list of accounts that are in
+        the local admin group that do not have appropriate access."""
         bad_admins = []
+        self.log_me.debug('Running local admin audit')
         # Generating admin exception data.
+        start = time()
         for data in self.local_admins:
             # Creating the dictionary for each iteration of local admin
             # data.
             bad_admin_data = {'host': data['host'], 'bad_admins': []}
-            # Matching the hostname to the config file to check against
-            # list of approved admins.
-            if data['host'] == self.config['local_admins'][data['host']]:
+            # Matching the hostname to the hosts in the config file to
+            # check against list of approved admins.
+            if data['host'] in self.config['local_admins'].keys():
                 # If the local admin is not in the approved list,
                 # append to the bad_admin key in the bad_admins_data
                 # dictionary.
@@ -273,16 +274,27 @@ class WinServerAudit(ADAudit):
                     )
                     if admin not in admin_list:
                         bad_admin_data['bad_admins'].append(admin)
+                        self.log_me.debug('%s is an unapproved admin' % admin)
+                    else:
+                        self.log_me.debug('%s is an approved admin' % admin)
+            else:
+                self.log_me.error('%s has no list of approved admins' % (
+                    data['host'])
+                )
             bad_admins.append(bad_admin_data)
         for bad_admin in bad_admins:
             # If there are bad admins, append the hostname and the list
             # of bad admins to the local_admin_ex attribute. Otherwise,
             # do nothing.
-            if len(bad_admin['bad_admins'] > 0):
+            if len(bad_admin['bad_admins']) > 0:
                 self.local_admin_ex.append(bad_admin)
                 # Let's create a log for bad local admins.  It is log
                 # worthy.
                 self.log_me.warning(
-                    '%s has unapproved local administrators',
-                    bad_admin['host']
+                    '%s has unapproved local administrators' % (
+                        bad_admin['host'])
                 )
+        end = time()
+        _elapsed = end - start
+        elapsed = int(round(_elapsed, 0))
+        self.log_me.debug('Admin audit completed in %d seconds' % elapsed)
--- tests/test_winaudit.py
+++ tests/test_winaudit.py
@@ -1,3 +1,6 @@
+from libs.winaudit import ADAudit, WinServerAudit
+
+
 class TestWinAudit:
     """WinAudit Test cases.
 
@@ -5,7 +8,20 @@ class TestWinAudit:
     TestPlaceHolder - A test case to act as a placeholder.  It has no
     actual value and should be removed once other test cases have been
     developed."""
-    def test_place_holder(self):
-        _int = 1
-        test = _int + 1
-        assert test == 2
+    def test_domain_admin_ex(self):
+        _class_test = ADAudit()
+        _class_test.config_file = 'test.conf'
+        _class_test.config.read(_class_test.config_file)
+        _class_test.domain_admins = ['bwayne', 'rgrayson', 'hdent']
+        _class_test.get_domain_admin_ex()
+        assert 'hdent' in _class_test.domain_admin_ex
+
+    def test_local_admin_ex(self):
+        _class_test = WinServerAudit()
+        _class_test.config_file = 'test.conf'
+        _class_test.config.read(_class_test.config_file)
+        _class_test.local_admins = [{'host': 'krypton',
+                                     'admins': ['kel', 'jel', 'dzod']}
+                                    ]
+        _class_test.get_local_admin_ex()
+        assert 'dzod' in _class_test.local_admin_ex[0]['bad_admins']
